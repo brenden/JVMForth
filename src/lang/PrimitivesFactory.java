bd2250056ee18c937f60cf5056417d4325249348
Brenden Kokoszka <bkokoszk@nd.edu>
2011-08-23 15:07:51 -0400

    further progress on lang class organization

--- src/lang/PrimitivesFactory.java
+++ src/lang/PrimitivesFactory.java
@@ -1,26 +1,13 @@
-public static class PrimitivesFactory {
-
-    private static Set<String> primitiveNames;
-
-    static {
-        String[] primitiveNamesArr = {"push", "swap", "do", "loop", "+loop", "begin", "until", ">r", "r>"};
-        primitiveNames = new HashSet<String>(Arrays.asList(primitiveNamesArr));
-    }
+public static class PrimitivesFactory { 
 
     private Word makePrimitive(String wordName) throws WordException {
 
-        if (primitiveNames.contains(wordName)) {
-
-            try {
-                Word primitiveWord = Class.forname(wordName).newInstance();
-                return primitiveWord;
-            }
-            catch (Throwable _) {
-                throw new WordException();
-            }
+        try {
+            Word primitiveWord = Class.forname(wordName).newInstance();
+            return primitiveWord;
         }
-        else {
+        catch (Throwable _) {
             throw new WordException();
         }
-    }
+    } 
 }
--- src/lang/RoutineWord.java
+++ src/lang/RoutineWord.java
@@ -1,15 +1,42 @@
 import org.objectweb.asm.*;
 
+//Any non-primitive word (user-defined or library)
 public class RoutineWord extends Word {
 
     private String name;       
-    private static Map<String, Map<int, Label>> routineLabels;
+
+    //A helper class which associates an execution token with a Label object
+    private class WordJump {
+        
+        private int executionToken;
+        private Label label;
+
+        WordJump(int executionToken, Label label) {
+        
+            this.executionToken = executionToken;
+            this.label = label;
+        }
+
+        int getExecutionToken() {
+
+            return executionToken;
+        }
+
+        Label getLabel() {
+
+            return label;
+        }
+    }
+
+    //These static fields coordinate the assignment of labels and execution tokens across words
+    private static Map<String, WordJump> routineLookup;
     private static int executionTokenCounter = 0;
 
     static {
-        routineLabels = new HashMap<String, Map<int, Label>>();
+        routineLookup = new HashMap<String, WordJump>();
     }
 
+    //If the word has been defined, create a new RoutineWord object for that word
     public RoutineWord(String routineName) throws WordException {
 
         if (rountineNames.contains(routineName)) {
@@ -20,12 +47,18 @@ public class RoutineWord extends Word {
         }
     }
 
+    //Define a new word. If the word was already defined, throw a WordException
     public static Label defineNewRoutine(String routineName) throws RoutineException {
 
-        if (!routineLabels.keySet().contains(routineName) {
-            Label routineLabel = new Label(routineName);
-            routineLabels.set(routineName, routineLabel);
+        if (!routineLookup.keySet().contains(routineName)) {
 
+            //Create a new WordJump; add it to routineLookup 
+            int executionToken = executionTokenCounter++;
+            Label routineLabel = new Label(routineName);
+            WordJump wordJump = new WordJump(executionToken, routineLabel);
+            routineLabels.set(routineName, wordJump);
+
+            //The new Label is passed back to the caller to be written in the word definition
             return routineLabel;
         }
         else {
@@ -33,6 +66,7 @@ public class RoutineWord extends Word {
         }
     }
 
+    //Write the appropriate jump to the MethodVisitor
     public MethodVisitor write(MethodVisitor mv) {
     }    
 }
