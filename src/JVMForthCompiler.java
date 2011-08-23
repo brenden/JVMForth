120267a7884c44ba60af622bc7f538b3d88ac179
Brenden Kokoszka <bkokoszk@nd.edu>
2011-08-23 15:08:30 -0400

    added logic for Word[] -> byte[] conversion

--- src/JVMForthCompiler.java
+++ src/JVMForthCompiler.java
@@ -1,8 +1,9 @@
+import org.objectweb.asm.*;
 import java.util.*;
 import java.util.regex.*;
 import java.io.*;
 
-public class JVMForthCompiler {
+public class JVMForthCompiler implements Opcodes {
 
     //Return the contents of the source file as a string
     private static String readSource(String sourceFile) throws IOException {
@@ -65,8 +66,32 @@ public class JVMForthCompiler {
         }
     }
 
+    //Create byte array of compiled output
+    private static byte[] byteify(Word[] words, String outputName) {
+
+        //Set up class for compiled output
+        ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_MAXS);
+        cw.visit(V1_6, ACC_PUBLIC, outputName, null, "java/lang/Object", null);
+        MethodVisitor mv = cw.visitMethod(ACC_PUBLIC+ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null);
+     
+        //Write each word's bytecode to the given class file
+        mv.visitCode();
+
+        for (Word word : words) {
+            mv = word.write(mv); 
+        }
+
+        mv.visitFrame(F_SAME, 0, null, 0, null);
+        mv.visitInsn(RETURN);
+        mv.visitMaxs(0, 0);
+        mv.visitEnd();
+
+        //Return ClassWriter as a byte array
+        byte[] contents = cw.toByteArray();
+        return contents;
+    }
+
     public static void main(String[] args) {
-        
-        
+            
     }
 }
